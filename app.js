7d9644609d6d413723b660d758ddd22f3c80110d
Nidhi Prajapati <65305905+nidhiprajapati2018@users.noreply.github.com>
2023-01-17 13:20:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+ const chalk = require('chalk');
+ const getNotes = require('./notes.js')
+
+// const fs = require('fs')
+// fs.writeFileSync('note.txt','My name is Nidhiprajapati')
+//  fs.appendFileSync('note.txt', '  I am live in Phoenix')
+
+//const add  = require('./utils.js')
+//const getNotes = require('./notes')
+// const sum = add(2,4)
+// console.log(sum)
+// const msg = getNotes();
+// console.log(msg)
+
+ //const validator = require('validator');
+// console.log(validator.isEmail('nidhi.p@gmail.com'))
+//console.log(validator.isURL('https://mead.io'))
+ 
+//  const greenmsg = chalk.bold.yellow.inverse("ERROR");
+//  console.log(greenmsg);
+// console.log(process.argv[2])
+
+const command = process.argv[2]
+
+if(command === 'add'){
+console.log("adding note")
+}else if(command === 'remove'){
+    console.log('removing note!')
+}
